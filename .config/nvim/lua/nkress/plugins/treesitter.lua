17ec57f6ae9d8542f244106105fbc03a39cf4b34
nkress <kressnick25@gmail.com>
2024-02-19 19:05:49 +1000

    setup function jumping with tree-sitter

--- .config/nvim/lua/nkress/plugins/treesitter.lua
+++ .config/nvim/lua/nkress/plugins/treesitter.lua
@@ -1,4 +1,34 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                textobjects = {
+                    move = {
+                        enable = true,
+                        set_jumps = true,
+                        goto_next_start = {
+                            ["<leader>nf"] = "@function.outer",
+                            ["<leader>nc"] = { query = "@class.outer", desc = "next class start" },
+                        },
+                        goto_previous_start = {
+                            ["<leader>pf"] = "@function.outer",
+                            ["<leader>pc"] = { query = "@class.outer", desc = "next class start" },
+                        },
+                        goto_next = {
+                            ["<leader>nn"] = "@conditional.outer",
+                        },
+                        goto_previous = {
+                            ["<leader>pp"] = "@conditional.outer",
+                        },
+                    },
+                },
+            })
+        end,
+    },
 }
