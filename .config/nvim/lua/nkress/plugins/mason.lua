874ee5197b7f13aa9d59e0ddd73dd307e5ac80be
Nicholas <kressnick25@gmail.com>
2024-02-21 17:49:22 +1000

    tweaks to LSP keybinds

--- .config/nvim/lua/nkress/plugins/mason.lua
+++ .config/nvim/lua/nkress/plugins/mason.lua
@@ -32,16 +32,14 @@ local function lspConfig()
 
     })
 
-	vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-	vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
+	-- vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+	-- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
 	-- Use LspAttach command to only map the following keys
 	-- after the language server attaches to the current buffer
 	vim.api.nvim_create_autocmd("LspAttach", {
 		group = vim.api.nvim_create_augroup("UserLspConfig", {}),
 		callback = function(ev)
-			-- enable completion triggered by <c-x><c-o>
-			vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
 			-- Buffer local mappings
 			local opts = { buffer = ev.buf }
@@ -50,18 +48,26 @@ local function lspConfig()
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-			vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
-			vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
-			vim.keymap.set("n", "<space>wl", function()
+			vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+			vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+			vim.keymap.set("n", "<leader>wl", function()
 				print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 			end, opts)
-			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+			vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+			vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
 			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-			vim.keymap.set("n", "<space>f", function()
-				vim.lsp.buf.format({ async = true })
-			end, opts) -- see `:help vim.lsp.*` for documentation on any of the below
+
+            -- formatting handled by null-ls
+            --
+			-- vim.keymap.set("n", "<space>f", function()
+			-- 	vim.lsp.buf.format({ async = true })
+			-- end, opts)
+
+            -- Handled by nvim-cmp.lua
+            --
+			-- enable completion triggered by <c-x><c-o>
+			-- vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 		end,
 	})
 end
