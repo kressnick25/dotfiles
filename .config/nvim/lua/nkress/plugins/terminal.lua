94d5520d05d3c389bc244c384f4f354895057265
nkress <kressnick25@gmail.com>
2024-04-18 20:52:41 +1000

    toggle powershell functions for windows only

--- .config/nvim/lua/nkress/plugins/terminal.lua
+++ .config/nvim/lua/nkress/plugins/terminal.lua
@@ -1,43 +1,38 @@
 return {
-    "akinsho/toggleterm.nvim",
-    tag = 'v2.10.0',
-    config = function()
-        local powershell_options = {
-            shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-            shellcmdflag =
-            "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-            shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-            shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-            shellquote = "",
-            shellxquote = "",
-        }
+	"akinsho/toggleterm.nvim",
+	tag = "v2.10.0",
+	config = function()
+		local powershell_options = {
+			shell = vim.fn.executable("pwsh") == 1 and "pwsh" or "powershell",
+			shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+			shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+			shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+			shellquote = "",
+			shellxquote = "",
+		}
 
-        for option, value in pairs(powershell_options) do
-            vim.opt[option] = value
-        end
+		if vim.fn.has("win32") then
+			for option, value in pairs(powershell_options) do
+				vim.opt[option] = value
+			end
+		end
 
-        require("toggleterm").setup({
-            hide_numbers = true,
-            open_mapping = [[<c-\>]],
-            direction = 'float',
-            float_opts = {
-                border = 'curved'
-            }
-        })
+		require("toggleterm").setup({
+			hide_numbers = true,
+			open_mapping = [[<c-\>]],
+			direction = "float",
+			float_opts = {
+				border = "curved",
+			},
+		})
 
-        -- lazygit specific terminal open
-        local Terminal = require('toggleterm.terminal').Terminal
-        local lazygit = Terminal:new({ cmd = 'lazygit', hidden = true })
-        function _lazygit_toggle()
-            lazygit:toggle()
-        end
+		-- lazygit specific terminal open
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
 
-        vim.api.nvim_set_keymap(
-            'n',
-            '<leader>g',
-            '<cmd>lua _lazygit_toggle()<CR>',
-            { noremap = true }
-        )
-    end
-
+		vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true })
+	end,
 }
