3f4b2b002947d6671f1d94d4cc901c8434f69cba
nkress <kressnick25@gmail.com>
2024-04-21 17:02:54 +1000

    telescope updates

--- .config/nvim/lua/nkress/plugins/telescope.lua
+++ .config/nvim/lua/nkress/plugins/telescope.lua
@@ -1,21 +1,75 @@
 return {
-    'nvim-telescope/telescope.nvim',
-    -- tag = '0.1.6',
-    branch = 'master',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    keys = {
-        { '<leader>ff', function() require('telescope.builtin').find_files() end, desc = 'Telescope find files' },
-        { '<leader>fg', function() require('telescope.builtin').live_grep() end,  desc = 'Telescope live grep' },
-        { '<leader>fb', function() require('telescope.builtin').buffers() end,    desc = 'Telescope buffers' },
-        { '<leader>fh', function() require('telescope.builtin').help_tags() end,  desc = 'Telescope help tags' },
-    },
-    opts = {
-        defaults = {
-            path_display = {
-                filename_first = {
-                    reverse_directories = false
-                }
-            }
-        }
-    }
+	"nvim-telescope/telescope.nvim",
+	-- tag = '0.1.6',
+	branch = "master",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("telescope.builtin").find_files()
+			end,
+			desc = "Telescope find files",
+		},
+		{
+			"<leader>fm",
+			function()
+				require("telescope.builtin").marks()
+			end,
+			desc = "Telescope find files",
+		},
+		{
+			"<leader>fg",
+			function()
+				require("telescope.builtin").live_grep()
+			end,
+			desc = "Telescope live grep",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("telescope.builtin").buffers()
+			end,
+			desc = "Telescope buffers",
+		},
+		{
+			"<leader>fh",
+			function()
+				require("telescope.builtin").help_tags()
+			end,
+			desc = "Telescope help tags",
+		},
+	},
+	config = function()
+		local telescope = require("telescope")
+		local telescopeConfig = require("telescope.config")
+
+		-- Clone the default Telescope configuration
+		local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+
+		-- I want to search in hidden/dot files.
+		table.insert(vimgrep_arguments, "--hidden")
+        -- remove leading whitespace from results output
+        table.insert(vimgrep_arguments, "")
+		-- I don't want to search in the `.git` directory.
+		table.insert(vimgrep_arguments, "--glob")
+		table.insert(vimgrep_arguments, "!**/.git/*")
+
+		telescope.setup({
+			defaults = {
+				vimgrep_arguments = vimgrep_arguments,
+				path_display = {
+					filename_first = {
+						reverse_directories = false,
+					},
+				},
+			},
+			pickers = {
+				find_files = {
+					-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+				},
+			},
+		})
+	end,
 }
