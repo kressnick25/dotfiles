1ee40d1257ff1e39ccb9b67ad28844c31654ddbc
Nicholas <kressnick25@gmail.com>
2025-02-03 19:05:01 +1000

    Replace toggleterm lazgit with real plugin

--- .config/nvim/lua/nkress/plugins/terminal.lua
+++ .config/nvim/lua/nkress/plugins/terminal.lua
@@ -22,14 +22,5 @@ return {
 			},
             shell = shell,
 		})
-
-		-- lazygit specific terminal open
-		local Terminal = require("toggleterm.terminal").Terminal
-		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-		function _lazygit_toggle()
-			lazygit:toggle()
-		end
-
-		vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true })
 	end,
 }
