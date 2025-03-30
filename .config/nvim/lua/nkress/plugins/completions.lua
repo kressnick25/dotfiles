462c9d8bd06d4e229642daea532e0de15c82195d
Nicholas <kressnick25@gmail.com>
2025-03-30 12:14:17 +1000

    use tab for completions

--- .config/nvim/lua/nkress/plugins/completions.lua
+++ .config/nvim/lua/nkress/plugins/completions.lua
@@ -34,7 +34,9 @@ return {
                     ["<C-f>"] = cmp.mapping.scroll_docs(4),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    -- Accept currently selected item.
+                    -- Set `select` to `false` to only confirm explicitly selected items.
+                    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
