8c1ae819ab8de98df817654a5729497581b3f9ec
Nicholas <kressnick25@gmail.com>
2025-03-30 13:01:52 +1000

    sync

--- .config/nvim/lua/nkress/plugins/none-ls.lua
+++ .config/nvim/lua/nkress/plugins/none-ls.lua
@@ -9,7 +9,6 @@ return {
 		null_ls.setup({
 			sources = {
 				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.autopep8,
                 null_ls.builtins.formatting.prettier
 			},
 		})
