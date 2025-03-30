fc5c0b8fff4dcb7439a2ac22aa7c133f8bc4e7b0
Nicholas <kressnick25@gmail.com>
2025-03-30 12:15:21 +1000

    Drop vim fugitive

--- .config/nvim/lua/nkress/plugins/terminal.lua
+++ .config/nvim/lua/nkress/plugins/terminal.lua
@@ -22,5 +22,14 @@ return {
 			},
             shell = shell,
 		})
+
+		-- lazygit specific terminal open
+		local Terminal = require("toggleterm.terminal").Terminal
+		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+		function _lazygit_toggle()
+			lazygit:toggle()
+		end
+
+		vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true })
 	end,
 }
