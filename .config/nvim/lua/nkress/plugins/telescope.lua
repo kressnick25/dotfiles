6041339165ea02c0d852519b128d2d565775ca0c
Nicholas <kressnick25@gmail.com>
2025-04-19 18:43:41 +1000

    fzf-lua does not ingore hidden

--- .config/nvim/lua/nkress/plugins/telescope.lua
+++ .config/nvim/lua/nkress/plugins/telescope.lua
@@ -1,44 +1,45 @@
 return {
-    "ibhagwan/fzf-lua",
-    -- optional for icon support
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    -- or if using mini.icons/mini.nvim
-    -- dependencies = { "echasnovski/mini.icons" },
-    opts = {
-        files = {
-           hidden = true,
-           rg_opts = [[--color=never --hidden --files -g "!.git"]]
-        }
-
-    },
-    keys = {
-        {
-            "<leader>ff",
-            function()
-                require("fzf-lua").files()
-            end,
-            desc = "Find files",
-        },
-        {
-            "<leader>fg",
-            function()
-                require("fzf-lua").live_grep_native()
-            end,
-            desc = "Live grep",
-        },
-        {
-            "<leader>fb",
-            function()
-                require("fzf-lua").buffers()
-            end,
-            desc = "Buffers",
-        },
-        {
-            "<leader>fh",
-            function()
-                require("fzf-lua").helptags()
-            end,
-            desc = "Help tags",
-        },
-    },
+	"ibhagwan/fzf-lua",
+	-- optional for icon support
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	-- or if using mini.icons/mini.nvim
+	-- dependencies = { "echasnovski/mini.icons" },
+	opts = {
+		files = {
+			hidden = true,
+		},
+		grep = {
+			hidden = true,
+		},
+	},
+	keys = {
+		{
+			"<leader>ff",
+			function()
+				require("fzf-lua").files()
+			end,
+			desc = "Find files",
+		},
+		{
+			"<leader>fg",
+			function()
+				require("fzf-lua").live_grep_native()
+			end,
+			desc = "Live grep",
+		},
+		{
+			"<leader>fb",
+			function()
+				require("fzf-lua").buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>fh",
+			function()
+				require("fzf-lua").helptags()
+			end,
+			desc = "Help tags",
+		},
+	},
 }
