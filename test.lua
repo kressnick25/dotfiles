65052fd07c00e0939f74c8ddd2b2b738d93dcf54
Nicholas <kressnick25@gmail.com>
2025-05-18 12:32:18 +1000

    WIP parsing params

--- test.lua
+++ test.lua
@@ -7,6 +7,16 @@ vim.fn.bufload(buf)
 local parser = vim.treesitter.get_parser(buf, 'groovy')
 print(parser)
 
+--- @param node TSNode?
+--- @return string
+local function node_text(node)
+    if node ~= nil then
+        return vim.treesitter.get_node_text(node, buf)
+    else
+        return "nil"
+    end
+end
+
 local function traverse_node(node, indent)
     indent = indent or 0
     local indent_str = string.rep("  ", indent)
@@ -18,6 +28,72 @@ local function traverse_node(node, indent)
     for child, _ in node:iter_children() do
         traverse_node(child, indent + 1)
     end
+end
+
+-- bloody lua indexes start at 1, but treesitter indexes start at 0
+
+-- @param s string
+-- @return string
+local function strip_surround(s)
+    local res
+    res = s:sub(2, -1)
+    res = res:sub(1, res:len() - 1)
+    return res
+end
+
+-- @param s string
+-- @return boolean
+local function is_in_quotes(s)
+    return s:sub(1, 1) == "'" or s:sub(1, 1) == "\""
+end
+
+--- @param node TSNode
+--- @return table
+local function parse_params(node)
+    local params = {}
+    if node:type() ~= "map" then
+        error("parse_params: unexpected node type: " .. node:type())
+    end
+
+    for _, key_value_pair_node in ipairs(node:named_children()) do
+        local key = node_text(key_value_pair_node:named_child(0))
+        local val = node_text(key_value_pair_node:named_child(1))
+        -- strip surrounding quotes
+        if is_in_quotes(key) then
+            key = strip_surround(key)
+        end
+        if is_in_quotes(val) then
+            val = strip_surround(val)
+        end
+        params[key] = val
+    end
+
+    return params
+end
+
+--- @param node TSNode
+--- @return table
+local function parse_named_parameter(node)
+    return {}
+end
+
+
+--- @param node TSNode
+--- @return table
+local function parse_named_params(node)
+    return {}
+end
+
+--- @param map table
+local function format_map(map)
+    local result = "{\n"
+
+    for k, v in pairs(map) do
+        result = result .. "\t'" .. k .. "' : '" .. v .. "',\n"
+    end
+
+    result = result .. "}"
+    return result
 end
 
 if parser ~= nil then
@@ -34,7 +110,7 @@ if parser ~= nil then
     end
 
 
-    print("Root has children: " .. tostring(has_children) .. " (count: " .. count .. ")")
+    -- print("Root has children: " .. tostring(has_children) .. " (count: " .. count .. ")")
     -- traverse_node(root)
 
     -- get all function calls with function name == "method"
@@ -45,45 +121,52 @@ if parser ~= nil then
     ]])
     for id, node, metdata in method_query:iter_captures(root, buf) do
         -- print(node:type())
-        print(vim.treesitter.get_node_text(node, buf))
-        local argument_list = node:child(1)
+        if node:type() == "function_call" then
+            -- print(vim.treesitter.get_node_text(node, buf))
+            local argument_list = node:child(1)
 
-        local def = {}
-        if argument_list ~= nil then
-            -- print(argument_list:type())
-            -- print(vim.treesitter.get_node_text(argument_list, buf))
+            local def = {}
+            if argument_list ~= nil then
+                -- print(argument_list:type())
+                -- print(vim.treesitter.get_node_text(argument_list, buf))
 
-            for arg in argument_list:iter_children() do
-                -- print(arg:type())
-                -- print(vim.treesitter.get_node_text(arg, buf))
-                local key_node = arg:child(0)
-                local val_node = arg:child(2)
-                if key_node ~= nil then
-                    local key = vim.treesitter.get_node_text(key_node, buf)
-                    -- print("key is " .. key)
-                    if val_node ~= nil then
-                        local val = vim.treesitter.get_node_text(val_node, buf)
-                        -- print("val is " .. val)
-                        if key == "params" then
-                            def[key] = val
-                            -- TODO parse key-val param map
-                        elseif key == "namedParams" then
-                            def[key] = val
-                            -- TODO parse list of name/type params
-                        else
+                for arg in argument_list:iter_children() do
+                    -- print(arg:type())
+                    -- print(vim.treesitter.get_node_text(arg, buf))
+                    local key_node = arg:child(0)
+                    local val_node = arg:child(2)
+                    if key_node ~= nil then
+                        -- @type string
+                        local key = vim.treesitter.get_node_text(key_node, buf)
+                        if val_node ~= nil then
+                            -- type string|table
+                            local val = nil
+                            if key == "params" then
+                                val = parse_params(val_node)
+                            elseif key == "namedParams" then
+                                val = parse_named_params(val_node)
+                            else
+                                val = vim.treesitter.get_node_text(val_node, buf)
+                                def[key] = val
+                            end
                             def[key] = val
                         end
                     end
                 end
             end
-        end
-        print("!!table def is")
-        for key, val in pairs(def) do
-            print("key: " .. key .. " val: " .. val)
+            print("!!table def is")
+            for key, val in pairs(def) do
+                if type(val) == "table" then
+                    print("key: " .. key .. " val: " .. format_map(val))
+                else
+                    print("key: " .. key .. " val: " .. val)
+                end
+            end
+            print("!! end table")
         end
 
 
-        -- break
+        break
     end
 end
 
