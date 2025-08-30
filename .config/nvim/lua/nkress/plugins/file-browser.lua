46bd17b8748d93108aba10f663976167dd24e932
Nicholas <kressnick25@gmail.com>
2025-08-30 10:36:41 +1000

    refactor: nvim-tree

--- .config/nvim/lua/nkress/plugins/file-browser.lua
+++ .config/nvim/lua/nkress/plugins/file-browser.lua
@@ -1,45 +1,12 @@
-local function configFn ()
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-
-    vim.opt.termguicolors = true
-
-    local function my_on_attach(bufnr)
-        local api = require 'nvim-tree.api'
-        vim.keymap.set('n', '<leader>tt', api.tree.toggle)
-        vim.keymap.set('n', '<leader>tf', api.tree.focus)
-        vim.keymap.set('n', '<leader>tb', function () api.tree.find_file({ update_root = true, open = true, focus = true }) end)
-        vim.keymap.set('n', '<leader>tc', function () api.tree.collapse_all() end)
-    end
-
-    local opts = {
-        filters = {
-            dotfiles = false,
-            custom = { '^.git$' },
-        },
-        hijack_cursor = true,
-        hijack_unnamed_buffer_when_opening = true,
-        renderer = {
-            highlight_git = 'none',
-            highlight_opened_files = 'none',
-            icons = {
-                git_placement = 'signcolumn',
-                show = {
-                    file = true,
-                    folder = false,
-                    folder_arrow = true,
-                    git = true
-                }
-            }
-        }
-    }
-
-    require('nvim-tree').setup(opts)
-
-    -- opts.on_attach does not seem to play nice with lazy
-    -- call here directly instead
-    my_on_attach()
+local function my_on_attach(bufnr)
+    local api = require 'nvim-tree.api'
+    vim.keymap.set('n', '<leader>tt', api.tree.toggle)
+    vim.keymap.set('n', '<leader>tf', api.tree.focus)
+    vim.keymap.set('n', '<leader>tb',
+        function() api.tree.find_file({ update_root = true, open = true, focus = true }) end)
+    vim.keymap.set('n', '<leader>tc', function() api.tree.collapse_all() end)
 end
+
 
 return {
     'nvim-tree/nvim-tree.lua',
@@ -47,5 +14,40 @@ return {
         -- requires a nerd font
         'nvim-tree/nvim-web-devicons',
     },
-    config = configFn
+    config = function()
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+
+        vim.opt.termguicolors = true
+
+        require('nvim-tree').setup({
+            filters = {
+                dotfiles = false,
+                custom = { '^.git$' },
+            },
+            hijack_cursor = true,
+            hijack_unnamed_buffer_when_opening = true,
+            view = {
+                side = "right",
+            },
+            renderer = {
+                highlight_git = 'none',
+                highlight_opened_files = 'none',
+                icons = {
+                    git_placement = 'signcolumn',
+                    show = {
+                        file = true,
+                        folder = false,
+                        folder_arrow = true,
+                        git = true
+                    }
+                }
+            }
+        })
+
+        -- opts.on_attach does not seem to play nice with lazy
+        -- call here directly instead
+        my_on_attach()
+    end
+
 }
