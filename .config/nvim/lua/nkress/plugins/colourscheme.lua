1054798c27ab6facf96b8a951a186a4b25aaf8de
Nicholas <kressnick25@gmail.com>
2025-08-30 10:27:38 +1000

    nvim colourscheme

--- .config/nvim/lua/nkress/plugins/colourscheme.lua
+++ .config/nvim/lua/nkress/plugins/colourscheme.lua
@@ -10,7 +10,17 @@ return {
             })
         end,
     },
-
+    {
+      "vague2k/vague.nvim",
+      lazy = false, -- make sure we load this during startup if it is your main colorscheme
+      priority = 1000, -- make sure to load this before all the other plugins
+      config = function()
+        -- NOTE: you do not need to call setup if you don't want to.
+        require("vague").setup({
+          -- optional configuration here
+        })
+      end
+    },
     {
         'crispybaccoon/evergarden',
         event = "VeryLazy",
